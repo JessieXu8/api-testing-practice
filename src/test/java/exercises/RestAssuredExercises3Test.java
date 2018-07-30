b0053b97ced17985b69662d9040e99205bf71ed3
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-30 13:57:14 +0800

    [jessie] use response specification in test 3

--- src/test/java/exercises/RestAssuredExercises3Test.java
+++ src/test/java/exercises/RestAssuredExercises3Test.java
@@ -1,12 +1,15 @@
 package exercises;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class RestAssuredExercises3Test {
 
@@ -42,8 +45,12 @@ public class RestAssuredExercises3Test {
 
 
     static void createResponseSpecification() {
-
-
+        responseSpec =
+                new ResponseSpecBuilder().
+                        expectStatusCode(200).
+                        expectContentType(ContentType.JSON).
+                        expectBody("MRData.CircuitTable.Circuits.circuitName[0]",equalTo("Albert Park Grand Prix Circuit")).
+                        build();
     }
 
     /*******************************************************
@@ -57,7 +64,7 @@ public class RestAssuredExercises3Test {
 
 
     static void getNinthDriverId() {
-
+//        ninthDriverId =
 
     }
 
@@ -74,8 +81,9 @@ public class RestAssuredExercises3Test {
 
         given().
                 spec(requestSpec).
-                when().
-                then();
+                when().get("/2014/1/circuits.json").
+                then().spec(responseSpec).
+                and().body("MRData.CircuitTable.Circuits[0].Location.locality",equalTo("Melbourne"));
     }
 
     /*******************************************************
